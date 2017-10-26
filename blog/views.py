2c15912f260e33cc3fb38919106d22992878cc02
lipingxin <lipingxin@outlook.com>
2017-10-26 21:19:46 +0800

    添加Markdown支持

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Post
+import markdown
 
 
 def index(request):
@@ -10,4 +11,10 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # markdown渲染detail
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc', ])
     return render(request, 'blog/detail.html', context={'post': post})
