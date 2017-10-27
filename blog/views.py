8923098abf03b5fdd8cb4a120f0cee21b38fe78b
lipingxin <lipingxin@outlook.com>
2017-10-27 16:45:01 +0800

    删除无用代码

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ import markdown
 
 
 def index(request):
-    posts = Post.objects.all().order_by('-create_time')
+    posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
@@ -20,19 +20,20 @@ def detail(request, pk):
     form = CommentForm()
     comments = post.comments_set.all()
     context = {'post': post,
-               'form':form,
-               'comments':comments}
+               'form': form,
+               'comments': comments}
     return render(request, 'blog/detail.html', context=context)
 
 
 def archives(request, year, month):
     posts = Post.objects.filter(create_time__year=year,
                                 create_time__month=month
-                                ).order_by('-create_time')
+                                )
+
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
 def categories(request, pk):
     category = get_object_or_404(Category, pk=pk)
-    posts = Post.objects.filter(category=category).order_by('-create_time')
+    posts = Post.objects.filter(category=category)
     return render(request, 'blog/index.html', context={'posts': posts})
