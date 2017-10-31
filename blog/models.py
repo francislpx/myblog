f2980256be3f2023ae1d7f9e312430b75e96fa55
lipingxin <lipingxin@outlook.com>
2017-10-31 10:45:34 +0800

    新增自动生成摘要功能，新增阅读量统计

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+import markdown
+from django.utils.html import strip_tags
 
 
 class Category(models.Model):
@@ -35,12 +37,33 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User)
     is_delete = models.IntegerField(default=1)
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.title if len(self.title) > 20 else self.title[:20]
 
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    # 重写save方法
+    def save(self, *args, **kwargs):
+        # 没有摘要的情况下
+        if not self.excerpt:
+            # 实例化一个Markdown类，渲染body
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将Markdown渲染成html
+            # strip_tags 去掉html里面的html标签，
+            # 然后截取54个字符赋值给excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        # 调用父类的save方法存储数据
+        super().save(*args, **kwargs)
 
     class Meta:
         ordering = ['-create_time']
