a1524224ead4b1d59ee1e6eb85ddeb3d553f4b32
lipingxin <lipingxin@outlook.com>
2017-11-08 15:28:07 +0800

    修改配置文件，增加允许访问的地址及static配置

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p1@lukl&xjhzvmbgfa3ujg*w*60gks68hzx11o*$77*=e5er1i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.lipingxin.com', '.lipingxin.top']
 
 
 # Application definition
@@ -124,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
